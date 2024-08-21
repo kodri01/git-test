0773a78225cc351704d5ff3f0b7d1481665297ca
kodri01 <kodrimukhtar5@gmail.com>
2024-08-21 20:15:48 +0700

    update gittag.py

--- gittag.py
+++ gittag.py
@@ -1,1 +1,10 @@
-test = 'git tag'
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def introduce(self):
+        print(f'Hai, Nama saya {self.name}, saya berumur {self.age} tahun')
+    
+Person1 = Person("Kodri", 24)
+Person1.introduce()
